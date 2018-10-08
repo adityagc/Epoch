ff480f3aa49a27a5cc156ea04eddf2a6878af101
Aditya Chindhade <adityagc@gmail.com>
2018-10-08 05:21:04 -0400

    Extract.py pushes directly to S3

--- src/Extract.py
+++ src/Extract.py
@@ -19,3 +19,26 @@ for index, row in df.iterrows():
 	symb = row['Symbol']
 	ts = TimeSeries(key=symb, datatype='csv')
 	data, meta_data = ts.get_intraday(symbol=symb,interval='1min', datatype='csv', outputsize='full')
+	
+
+# Writing to S3 bucket
+conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY)
+
+def percent_complete(complete, total):
+    sys.stdout.write('.')
+    sys.stdout.flush()
+
+bucket_name = "insightde2018bucket"
+bucket = s3_connection.get_bucket('bucket_name')
+key = boto.s3.key.Key(bucket, 'industry-comp.csv')
+with open('industry-comp.csv') as f:
+    key.send_file(f)
+
+for index, row in df.iterrows():
+    symb = row['Symbol']
+    filepath = os.path.join(dir_name, str(symb) + "." + format)
+    close.to_csv( +"_new.csv")
+    key.set_contents_from_filename(testfile,
+    cb=percent_cb, num_cb=10)
+    print 'Uploading %s to Amazon S3 bucket %s'
